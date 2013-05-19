3d2a2103fe87e4ddb44e94bed3d1c7e9874af29d
zivfishfedder <fishfederationz@gmail.com>
2013-05-18 21:52:30 -0400

    adding

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Mobiletest::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match 'details' => 'main#details'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
